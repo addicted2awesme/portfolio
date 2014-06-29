8f1bc2bab0cfea24c96ad4af8d5887164ee5fdc4
Owen Spencer <owen@owenlovesawesome.com>
2014-06-28 17:53:05 -0700

    getting shit done

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,27 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top-200
+          scrollTop: target.offset().top-100
         }, 500);
         return false;
       }
     }
   });
+});
+
+$(document).ready(function(){
+	
+	$("#whopper").click(function() {
+/* 		$("nav").toggle(200); */
+		$("nav").addClass("active");
+		$("nav").slideDown(300);
+		if(condition) {
+			// code to be executed if condition is true
+		} 
+		else {
+			// code to be executed if condition is false
+		}
+		
+
+	});
 });
